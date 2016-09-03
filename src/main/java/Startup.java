c38fe1bfd11344bb0ca02cf2621bf1ab109d9b0c
N00bface <vmjari@hotmail.com>
2016-09-03 15:15:38 +0200

    initial commit

---
+++ src/main/java/Startup.java
@@ -0,0 +1,15 @@
+/**
+ * @author Jari Van Melckebeke
+ * @since 02.09.16
+ */
+public class Startup {
+    /**
+     * main function:
+     * This function will control the backend of the graph generation
+     *
+     * @param args <i>Not used</i>
+     */
+    public static void main(String[] args) {
+        
+    }
+}
