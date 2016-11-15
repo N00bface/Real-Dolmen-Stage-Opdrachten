f0bfcacf02882814d44d8bae2271ea2a5c9aa384
N00bface <vmjari@hotmail.com>
2016-11-15 17:00:59 +0100

    working on Codeship

--- src/main/java/org/jarivm/relationGraph/Application.java
+++ src/main/java/org/jarivm/relationGraph/Application.java
@@ -38,8 +38,6 @@ public class Application extends Neo4jConfiguration {
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/springstageopdracht?autoReconnect=true&useSSL=false");
-		driverManagerDataSource.setUsername("root");
-		driverManagerDataSource.setPassword("Tanzania1");
 		return driverManagerDataSource;
 	}
 
