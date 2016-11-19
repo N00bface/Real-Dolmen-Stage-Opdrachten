2ab8515d54cdabdcf27cb918ce780e4cb8058684
N00bface <vmjari@hotmail.com>
2016-11-19 19:49:50 +0100

    moving to travis

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-sudo apt-get install google-chrome-stable
+wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip -P chrome/
+unzip chrome/chromedriver_linux64.zip -d chrome/
+chmod +x chrome/chromedriver
+sudo mv -f chrome/chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
 wget -O neo4j-community-3.0.1.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-3.0.1-unix.tar.gz
 tar -xzvf neo4j-community-3.0.1.tar.gz -o neo4j-community-3.0.1/
 neo4j-community-3.0.1/bin/neo4j start
 sleep 20
 curl -H "Content-Type: application/json" -X POST -d '{"password":"tanzania"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
 ./neo4j-community-3.0.1/bin/neo4j-shell -c < setup.cql
-#ddd
