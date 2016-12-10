6768c3ac47ddc2310004f6676b2fb663cc027806
N00bface <vmjari@hotmail.com>
2016-12-10 20:19:47 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ chmod +x chrome/chromedriver
 sudo mv -f chrome/chromedriver /usr/local/share/chromedriver
 sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
-
-wget -O neo4j-community-3.0.1.tar.gz "http://neo4j.com/artifact.php?name=neo4j-community-3.0.1-unix.tar.gz"
-unzip neo4j-community-3.0.1.tar.gz neo4j-community-3.0.1/
-neo4j-community-3.0.1/bin/neo4j start
+wget https://neo4j.com/artifact.php?name=neo4j-community-3.0.3-unix.tar.gz -o neo4j.tar.gz
+tar -xf neo4j.tar.gz
+neo4j/bin/neo4j start
 sleep 20
 curl -H "Content-Type: application/json" -X POST -d '{"password":"tanzania"}' -u neo4j:neo4j http://localhost:7474/browser
 ./neo4j-community-3.0.1/bin/neo4j-shell -c < setup.cql
