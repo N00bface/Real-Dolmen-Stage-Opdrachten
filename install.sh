8ecba1f50e8879e6bc79e420e63319df38994d86
N00bface <vmjari@hotmail.com>
2016-12-10 19:55:04 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ wget -O neo4j-community-3.0.1.tar.gz http://neo4j.com/artifact.php?name=neo4j-co
 tar -xzvf neo4j-community-3.0.1.tar.gz -o neo4j-community-3.0.1/
 neo4j-community-3.0.1/bin/neo4j start
 sleep 20
-curl -H "Content-Type: application/json" -X POST -d '{"password":"tanzania"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+curl -H "Content-Type: application/json" -X POST -d '{"password":"neo4j"}' -u neo4j:neo4j http://localhost:7474/browser
 ./neo4j-community-3.0.1/bin/neo4j-shell -c < setup.cql
