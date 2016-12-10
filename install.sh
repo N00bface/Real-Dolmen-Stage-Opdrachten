050ca39910dcbb974477bd6055157a0a7661cd94
N00bface <vmjari@hotmail.com>
2016-12-10 20:02:14 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 #sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 
 wget -O neo4j-community-3.0.1.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-3.0.1-unix.tar.gz
+mkdir neo4j-community-3.0.1/
 tar -xzvf neo4j-community-3.0.1.tar.gz -o neo4j-community-3.0.1/
 neo4j-community-3.0.1/bin/neo4j start
 sleep 20
