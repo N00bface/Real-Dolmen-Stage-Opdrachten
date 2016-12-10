9f3c009d0d28928362ed1f8f9f88195ed918f65f
N00bface <vmjari@hotmail.com>
2016-12-10 20:11:13 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-#wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip -P chrome/
-#unzip chrome/chromedriver_linux64.zip -d chrome/
-#chmod +x chrome/chromedriver
-#sudo mv -f chrome/chromedriver /usr/local/share/chromedriver
-#sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
-#sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip -P chrome/
+unzip chrome/chromedriver_linux64.zip -d chrome/
+chmod +x chrome/chromedriver
+sudo mv -f chrome/chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 
 wget -O neo4j-community-3.0.1.tar.gz "http://neo4j.com/artifact.php?name=neo4j-community-3.0.1-unix.tar.gz"
-mkdir neo4j-community-3.0.1/
 sudo tar -xzvf neo4j-community-3.0.1.tar.gz -o neo4j-community-3.0.1/
 neo4j-community-3.0.1/bin/neo4j start
 sleep 20
