85565c166faa5c3386446890e923de0e03ddb8ce
N00bface <vmjari@hotmail.com>
2016-12-13 16:37:08 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget "https://neo4j.com/artifact.php?name=neo4j-community-3.1.0-unix.tar.gz"
-tar -xzvf "artifact.php\?name=neo4j-community-3.1.0-unix.tar.gz"
+tar -xzvf artifact.php\?name=neo4j-community-3.1.0-unix.tar.gz
 echo "starting neo4j"
 neo4j-community-3.1.0/bin/neo4j start
 sleep 10
