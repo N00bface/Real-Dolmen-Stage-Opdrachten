19e2db611fa6b722784d237c3e1bc3f110875281
N00bface <vmjari@hotmail.com>
2016-12-13 18:57:27 +0100

    custom test integration

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget "https://neo4j.com/artifact.php?name=neo4j-community-3.1.0-unix.tar.gz"
 tar -xzvf artifact.php\?name=neo4j-community-3.1.0-unix.tar.gz
 echo "starting neo4j"
 neo4j-community-3.1.0/bin/neo4j start
-sleep 10
+sleep 15
 curl -v -H "Content-Type: application/json" -X POST -d '{"password":"tanzania"}' -u neo4j:neo4j http://localhost:7474/browser
 echo "done sleeping"
 ./neo4j-community-3.1.0/bin/neo4j-shell -f setup.cql
